c3807c1b8412dbd89c4afb66efd2f38e7364c474
Lukas Palmer <Lukas Palmer>
2016-12-30 20:37:56 -0500

    tutorial comments

--- main.js
+++ main.js
@@ -41,35 +41,43 @@ function setup() {
   canvas = document.getElementById("glCanvas");
   gl = canvas.getContext("experimental-webgl", {antialias: true}); // get a WebGL context
 
-  gl.clearColor(.05, .23, .3, 1);
+  gl.clearColor(.05, .23, .3, 1); // set the background color to clear with
 
+  // set up GLSL program & extract variable IDs
   program = createProgram(glslVertexSource, glslFragmentSource);
   unfmScale = gl.getUniformLocation(program, "scale");
   unfmAspect = gl.getUniformLocation(program, "aspect");
   attrPosition = gl.getAttribLocation(program, "position");
   attrColor = gl.getAttribLocation(program, "color");
+
   gl.enableVertexAttribArray(attrPosition);
   gl.enableVertexAttribArray(attrColor);
+
   gl.useProgram(program);
 
+  // create a buffer on the GPU and upload the triangle mesh to it
   triangleBuffer = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, triangleBuffer);
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(triangleMesh), gl.STATIC_DRAW);
 
+  // tell our shader the scale we want
   gl.uniform1f(unfmScale, 2.2);
 
-  draw(); // start the draw loop
+  // start the draw loop
+  draw();
 }
 
 function draw() {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
 
+  // set the aspect ratio
   gl.uniform1f(unfmAspect, canvas.width / canvas.height);
 
+  // draw to the whole canvas and clear to the background color
   gl.viewport(0, 0, canvas.width, canvas.height);
   gl.clear(gl.COLOR_BUFFER_BIT);
-
+  
   gl.bindBuffer(gl.ARRAY_BUFFER, triangleBuffer);
   gl.vertexAttribPointer(attrPosition, 2, gl.FLOAT, false, 4 * (2 + 3), 0);
   gl.vertexAttribPointer(attrColor, 3, gl.FLOAT, false, 4 * (2 + 3), 4 * 2);
@@ -83,9 +91,12 @@ function createShader(source, type) {
   var shader = gl.createShader(type);
   gl.shaderSource(shader, source);
   gl.compileShader(shader);
+
+  // report any errors from compiling the shader
   if(!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
     alert("error compiling " + (type == gl.VERTEX_SHADER ? "vertex" : "fragment") + " shader: " + gl.getShaderInfoLog(shader));
   }
+
   return shader;
 }
 
