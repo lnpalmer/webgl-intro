13e7bd071af1af483394e17f7df08cf22dd2ba73
Lukas Palmer <Lukas Palmer>
2017-01-10 13:13:57 -0500

    webgl

--- main.js
+++ main.js
@@ -6,12 +6,9 @@ var attrColor;
 var unfmScale;
 var unfmAspect;
 var triangleMesh = [
-  -1, -.866,
-  .2, .6, 1,
-  1, -.866,
-  .6, 1, .2,
-  0, 1,
-  1, .2, .6
+  -1, -.866, .2, .6, 1,
+  1, -.866, .6, 1, .2,
+  0, 1, 1, .2, .6
 ];
 var triangleBuffer;
 
@@ -43,6 +40,11 @@ function setup() {
 
   gl.clearColor(.05, .23, .3, 1); // set the background color to clear with
 
+  // create a buffer on the GPU and upload the triangle mesh to it
+  triangleBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, triangleBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(triangleMesh), gl.STATIC_DRAW);
+
   // set up GLSL program & extract variable IDs
   program = createProgram(glslVertexSource, glslFragmentSource);
   unfmScale = gl.getUniformLocation(program, "scale");
@@ -54,11 +56,6 @@ function setup() {
   gl.enableVertexAttribArray(attrColor);
 
   gl.useProgram(program);
-
-  // create a buffer on the GPU and upload the triangle mesh to it
-  triangleBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, triangleBuffer);
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(triangleMesh), gl.STATIC_DRAW);
 
   // tell our shader the scale we want
   gl.uniform1f(unfmScale, 2.2);
@@ -77,7 +74,7 @@ function draw() {
   // draw to the whole canvas and clear to the background color
   gl.viewport(0, 0, canvas.width, canvas.height);
   gl.clear(gl.COLOR_BUFFER_BIT);
-  
+
   gl.bindBuffer(gl.ARRAY_BUFFER, triangleBuffer);
   gl.vertexAttribPointer(attrPosition, 2, gl.FLOAT, false, 4 * (2 + 3), 0);
   gl.vertexAttribPointer(attrColor, 3, gl.FLOAT, false, 4 * (2 + 3), 4 * 2);
